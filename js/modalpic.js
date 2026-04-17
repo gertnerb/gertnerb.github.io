9dcb6e0d9e24778c2a104834169123c72741debc
Balazs Gertner <gertnerb@gmail.com>
2026-04-17 15:52:41 +0200

    new update

--- js/modalpic.js
+++ js/modalpic.js
@@ -1,12 +1,13 @@
 const images = document.querySelectorAll(".img-click");
 const modal = document.getElementById("imageModal");
-const modalImg = document.getElementById("modalImg");
+const modalImg = document.getElementById("modalImage");
 const close = document.querySelector(".modal-close");
 
 images.forEach(img => {
   img.onclick = function () {
     modal.style.display = "block";
-    modalImg.src = this.src.replace("600/400", "1200/800");
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
   }
 });
 
